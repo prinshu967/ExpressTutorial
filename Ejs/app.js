bb5e4f325f34e38eac90dff813503fdb97880729
Prinshu Kumar <118058374+prinshu967@users.noreply.github.com>
2025-05-22 00:36:19 +0530

    Update app.js

--- Ejs/app.js
+++ Ejs/app.js
@@ -19,21 +19,7 @@ app.get('/index', (req, res) => {
   res.render('index', { user: 'Prinshu' });
 });
 
-// Optional error handler
-// app.get('/about', (req, res, next) => {
-//   return next(new Error('Something went wrong!'));
-// });
 
-// app.use((err, req, res, next) => {
-//   console.error(err.stack);
-//   res.status(500).send('Something broke!');
-// });
-
-// Dynamic Route Example
-app.get('/user/:id', (req, res) => {
-  const userId = req.params.id;
-  res.send(`User Name: ${userId}`);
-});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
